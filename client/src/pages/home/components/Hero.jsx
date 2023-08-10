77e82803df96478535fed92f919dd8774c543976
The Ace <udonsichisom02@gmail.com>
2023-08-10 09:58:36 +0100

    feat: completed hero component

--- client/src/pages/home/components/Hero.jsx
+++ client/src/pages/home/components/Hero.jsx
@@ -1,4 +1,22 @@
+import video from "../../../assets/videos/video.mp4";
+import Button from "../../../components/ui/Button";
+import { Link } from "react-router-dom";
+
 function Hero() {
-  return <h1>Hero</h1>;
+  return (
+    <section className="hero">
+      <h1>
+        Discover, <span>Connect</span>, and Navigate the Frontiers of Technology
+      </h1>
+      <p>
+        Explore and discover a limitless world filled with endless opportunities
+        through the leveraging of new and exciting technologies
+      </p>
+      <Link to="/signup">
+        <Button>Register Now</Button>
+      </Link>
+      <video src={video} loop autoPlay aria-hidden="true"></video>
+    </section>
+  );
 }
 export default Hero;
