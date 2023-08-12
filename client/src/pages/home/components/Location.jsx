dd42c2e2f0588dcf99de4a4aca890f6fdee9e283
The Ace <udonsichisom02@gmail.com>
2023-08-12 10:43:01 +0100

    chore: updated assets

--- client/src/pages/home/components/Location.jsx
+++ client/src/pages/home/components/Location.jsx
@@ -1,4 +1,4 @@
-import flier from "../../../assets/images/flier.png";
+import techverse from "../../../assets/images/techverse.png";
 
 function Location() {
   return (
@@ -13,7 +13,7 @@ function Location() {
             around
           </p>
         </div>
-        <img src={flier} alt="" />
+        <img src={techverse} alt="" />
       </div>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.1955598720333!2d6.99189007383898!3d5.387137035327012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10425d762018584d%3A0x4ffe8b989faa3ba!2sUCC%20Building%20Futo!5e0!3m2!1sen!2sng!4v1691797959048!5m2!1sen!2sng"
